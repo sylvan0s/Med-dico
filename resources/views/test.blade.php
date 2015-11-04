b0556e2c7e795064d12b0c71ebec022736db0cd9
xavier <xavier@leaneous.com>
2015-11-04 15:22:32 +0100

    test

---
+++ resources/views/test.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<!--[if lte IE 7]> <html class="ie7" lang="fr"> <![endif]-->
+<!--[if IE 8]> <html class="ie8" lang="fr"> <![endif]-->
+<!--[if IE 9]> <html class="ie9" lang="fr"> <![endif]-->
+<html lang="fr">
+<head>
+    @include('includes.head')
+</head>
+
+<body>
+<a href="#" class="header-controls menu"><i class="fa fa-bars"></i></a>
+<a href="#" class="header-controls close"><i class="fa fa-times"></i></a>
+
+<header id="website-header">
+    @include('includes.header')
+</header>
+
+
+
+<footer id="website-footer">
+    @include('includes.footer')
+</footer>
+
+</body>
+</html>
